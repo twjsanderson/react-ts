f7babe806f2b50128f06c23ac089e8e01de018d0
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2022-01-03 23:25:44 -0500

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import './App.css';
+import * as React from 'react';
 
-function App() {
+interface DataObject {
+  id: number;
+  text: {
+    name: string;
+    thing?: number;
+  };
+}
+
+interface DataArray extends Array<DataObject> {}
+
+const App = () => {
+  const data: DataArray = [
+    {
+      id: 1,
+      text: {
+        name: 'tom'
+      }
+    }
+  ]
   return (
-    <div>
+    <div className='flex flex-col'>
       <h1>Test</h1>
+      {
+        data.map(item => {
+          return <p key={item.id}>{item.text.name}</p>
+        })
+      }
+      <p>butts</p>
     </div>
   );
 }
